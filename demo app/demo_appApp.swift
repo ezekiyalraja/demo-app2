93b170b0cf23daa4470b10b7c54d25318f73ce36
techunity <ezekiyalraja1998@gmail.com>
2023-01-13 03:37:15 -0800

    Initial Commit

---
+++ demo app/demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_appApp.swift
+//  demo app
+//
+//  Created by techunity on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
