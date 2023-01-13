e9d0fc3ee0341518d5b9291da76e6c151a73fb58
techunity <ezekiyalraja1998@gmail.com>
2023-01-13 04:34:35 -0800

    1

--- demo app/ContentView.swift
+++ demo app/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            print("rrr")
+            print("G")
+            
+            
+            
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
